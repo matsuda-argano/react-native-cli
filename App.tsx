01e29b0a98c9fcb9ca23126ff31e9dff04051db7
kazuki827 <xm.kazuki827@gmail.com>
2022-07-17 17:49:03 +0900

    moving between screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button title="詳細へ" onPress={() => navigation.navigate('Details')} />
     </View>
   );
 }
