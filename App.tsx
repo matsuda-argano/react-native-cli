cad165927a8e9dc5bc3a364859a9dc1674a6598c
kazuki827 <xm.kazuki827@gmail.com>
2022-07-17 20:30:14 +0900

    passing parameters to routes

--- App.tsx
+++ App.tsx
@@ -7,15 +7,36 @@ function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
-      <Button title="詳細へ" onPress={() => navigation.navigate('Details')} />
+      <Button
+        title="詳細へ"
+        onPress={() => {
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({route, navigation}) {
+  const {itemId, otherParam} = route.params;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Text>{JSON.stringify(itemId)}</Text>
+      <Text>{JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => {
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          });
+        }}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
